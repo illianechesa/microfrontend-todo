3147a5daf26fbdc34ddc6d368aeefed03a2a2d35
Illia Nechesa <illia.nechesa@eos-eng.com>
2021-01-08 12:09:05 +0100

    add routing to different applications
    Signed-off-by: Illia Nechesa <illia.nechesa@eos-eng.com>

---
+++ root-config/src/activity-functions.js
@@ -0,0 +1,18 @@
+export function prefix(location, ...prefixes) {
+  return prefixes.some(
+    (prefix) => location.href.indexOf(`${location.origin}/${prefix}`) !== -1
+  );
+}
+
+export function nav() {
+  // The nav is always active
+  return true;
+}
+
+export function taskList(location) {
+  return prefix(location, "task-list");
+}
+
+export function createTask(location) {
+  return prefix(location, "create-task");
+}
--- root-config/src/eos-root-config.js
+++ root-config/src/eos-root-config.js
@@ -1,20 +1,18 @@
 import { registerApplication, start } from "single-spa";
+import * as isActive from "./activity-functions";
 
-registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
-  activeWhen: ["/"],
-});
+registerApplication("@eos/nav", () => System.import("@eos/nav"), isActive.nav);
 
-// registerApplication({
-//   name: "@eos/navbar",
-//   app: () => System.import("@eos/navbar"),
-//   activeWhen: ["/"]
-// });
+registerApplication(
+  "@eos/task-list",
+  () => System.import("@eos/task-list"),
+  isActive.taskList
+);
 
-start({
-  urlRerouteOnly: true,
-});
+registerApplication(
+  "@eos/create-task",
+  () => System.import("@eos/create-task"),
+  isActive.createTask
+);
+
+start();
