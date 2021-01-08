d4721bb092720383d13bac09fb793512c15940a0
Illia Nechesa <illia.nechesa@eos-eng.com>
2021-01-08 12:20:23 +0100

    add containers for the applications
    Signed-off-by: Illia Nechesa <illia.nechesa@eos-eng.com>

--- root-config/src/eos-root-config.js
+++ root-config/src/eos-root-config.js
@@ -1,18 +1,22 @@
 import { registerApplication, start } from "single-spa";
 import * as isActive from "./activity-functions";
 
-registerApplication("@eos/nav", () => System.import("@eos/nav"), isActive.nav);
+registerApplication("@eos/nav", () => System.import("@eos/nav"), isActive.nav, {
+  domElement: document.getElementById("nav-container"),
+});
 
 registerApplication(
   "@eos/task-list",
   () => System.import("@eos/task-list"),
-  isActive.taskList
+  isActive.taskList,
+  { domElement: document.getElementById("task-list-container") }
 );
 
 registerApplication(
   "@eos/create-task",
   () => System.import("@eos/create-task"),
-  isActive.createTask
+  isActive.createTask,
+  { domElement: document.getElementById("create-task-container") }
 );
 
 start();
