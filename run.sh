8007160839b8d3a679a70e7e043abb4d52bfc9d8
Illia Nechesa <illia.nechesa@eos-eng.com>
2021-01-08 14:22:12 +0100

    add run script in order to execute all at once
    Signed-off-by: Illia Nechesa <illia.nechesa@eos-eng.com>

--- run.sh
+++ run.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-cd root-config && yarn start 
-cd ..
-cd nav && yarn start --port 9001
-cd ..
-cd task-list && yarn start --port 9002
-cd .. 
-cd create-task && yarn start --port 9003
-cd ..
+xterm -title "Root" -e "cd root-config && yarn start" &
+xterm -title "Nav" -e "cd nav && yarn start --port 9001" &
+xterm -title "List" -e "cd task-list && yarn start --port 9002" &
+xterm -title "Create" -e "cd create-task && yarn start --port 9003" &
 xdg-open http://localhost:9000
