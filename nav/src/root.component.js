5914e260d89814f12617290782e8c3cad23d9994
Illia Nechesa <illia.nechesa@eos-eng.com>
2021-01-08 12:42:34 +0100

    add styles to micro applications
    Signed-off-by: Illia Nechesa <illia.nechesa@eos-eng.com>

--- nav/src/root.component.js
+++ nav/src/root.component.js
@@ -1,5 +1,15 @@
 import React from "react";
+import "./root.component.css";
 
-export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+export default function Root() {
+  return (
+    <nav className="nav">
+      <a href="/task-list" className="link">
+        Task List
+      </a>
+      <a href="/create-task" className="link">
+        Create Task
+      </a>
+    </nav>
+  );
 }
