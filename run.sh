09e70c969a89e59bbf2f3808e18fc9c0c6317e47
Illia Nechesa <illia.nechesa@eos-eng.com>
2021-01-11 08:23:17 +0100

    update run script to use bash instead of xterm
    Signed-off-by: Illia Nechesa <illia.nechesa@eos-eng.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-xterm -title "Root" -e "cd root-config && yarn start" &
-xterm -title "Nav" -e "cd nav && yarn start --port 9001" &
-xterm -title "List" -e "cd task-list && yarn start --port 9002" &
-xterm -title "Create" -e "cd create-task && yarn start --port 9003" &
+gnome-terminal --title "Root Config" --execute bash -c "cd root-config && yarn start" 
+gnome-terminal --title "Navigation" --execute bash -c "cd nav && yarn start --port 9001" 
+gnome-terminal --title "Task List" --execute bash -c "cd task-list && yarn start --port 9002" 
+gnome-terminal --title "Create Task" --execute bash -c "cd create-task && yarn start --port 9003" 
 xdg-open http://localhost:9000
