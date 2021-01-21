7c5fc9163b6643e0c20ab5a8192d977e2b7e37b7
Illia Nechesa <illia.nechesa@eos-eng.com>
2021-01-21 09:51:05 +0100

    update task creation form interface
    Signed-off-by: Illia Nechesa <illia.nechesa@eos-eng.com>

--- create-task/src/components/Form.js
+++ create-task/src/components/Form.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
-
+import "./Form.css";
 export class Form extends Component {
   render() {
     return (
-      <form>
-        <p>
-          Name: <input type="text" name="title" />
-        </p>
-        <p>
-          Description: <input type="text" name="description" />
-        </p>
-        <p>
-          Due date: <input type="date" name="date" />
-        </p>
-        <input type="submit" value="Submit" />
-      </form>
+      <div className="container">
+        <form>
+          <p>
+            Name: <input type="text" name="title" />
+          </p>
+          <p>
+            Description: <input type="text" name="description" />
+          </p>
+          <p>
+            Due date: <input type="date" name="date" />
+          </p>
+          <input type="submit" value="Add" />
+          <input type="reset" value="Reset"></input>
+        </form>
+      </div>
     );
   }
 }
